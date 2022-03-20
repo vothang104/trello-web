65dcfb9e315a3bbd8d028097c90a17ef68eb6259
vothang104 <19130203@st.hcmuaf.edu.vn>
2022-03-21 00:17:01 +0700

    clean architecture

---
+++ src/components/boardcontent/BoardContent.js
@@ -0,0 +1,13 @@
+import Column from 'components/column/Column'
+import React from 'react'
+import './BoardContent.scss'
+
+function BoardContent() {
+  return (
+    <div className="board-columns">
+      <Column></Column>
+    </div>
+  )
+}
+
+export default BoardContent
