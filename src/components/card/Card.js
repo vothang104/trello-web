6018ca8922994d2456435cd601ea9e3e1b7d798d
vothang104 <19130203@st.hcmuaf.edu.vn>
2022-03-29 23:26:59 +0700

    drag and drop

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types'
 
 function Card({ card }) {
   return (
-    <li className="item">
+    <div className="item card-drag-handle">
       {
       card.cover && 
-      <img className='img'
+      <img draggable='false' className='img'
       src={card.cover} alt="" />
       }
       <span className='text'>{card.title}</span>
-    </li>
+    </div>
   )
 }
 Card.propTypes = {
--- src/components/column/Column.js
+++ src/components/column/Column.js
@@ -3,26 +3,44 @@ import React from 'react'
 import { mapOrder } from 'util/sort';
 import { v4 } from 'uuid';
 import './Column.scss'
+import { Container, Draggable } from 'react-smooth-dnd'
 
 function Column({ column }) {
   const cards = column.cards;
   if(cards) {
     mapOrder(cards, column.cardOrder, 'id')
   }
+  const onColumnDrop = (dropResult) => {
+    console.log('Card drop ~ ', dropResult);
+  }
   return (
     <div className="column">
-      <h3 className='title'>{column.title}</h3>
-      <ul className="list-todo">
+      <h3 className='title column-drag-handle'>{column.title}</h3>
+      <div className="list-todo">
+      <Container
+      orientation='vertical'
+      groupName='column'
+      onDrop={onColumnDrop}
+      dragHandleSelector=".card-drag-handle"
+      getChildPayload={index => cards[index]}
+      dropPlaceholder={{
+        animationDuration: 150,
+        showOnTop: true,
+        className: 'cards-drop-preview'
+      }}
+      >
         {
           cards && cards.length > 0 &&
           cards.map(card => (
+          <Draggable key={v4()}>
             <Card
             card={card}
-            key={v4()}
             ></Card>
+          </Draggable>
           ))
         }
-      </ul>
+        </Container>
+      </div>
       <div className="todo-add">
         <input placeholder='Filling todo' type="text" />
         <button>Add</button>
