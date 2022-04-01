860aca37c235202644f55c6ded1ed61957c67175
vothang104 <19130203@st.hcmuaf.edu.vn>
2022-04-01 11:37:38 +0700

    handle state change

--- src/components/boardcontent/BoardContent.js
+++ src/components/boardcontent/BoardContent.js
@@ -7,6 +7,7 @@ import { isEmpty } from 'lodash'
 import { v4 } from 'uuid'
 import { mapOrder } from 'util/sort'
 import { Container, Draggable } from 'react-smooth-dnd'
+import { applyDrag } from 'util/dragDrop'
 
 function BoardContent() {
   const [boards, setBoards] = useState({})
@@ -25,7 +26,25 @@ function BoardContent() {
   if(isEmpty(boards)) return <div className='not-found'>Board not found</div>
 
   const onColumnDrop = (dropResult) => {
-    console.log(dropResult);
+    console.log('drop result ~ ', dropResult);
+    const cloneBoards = {...boards};
+    let cloneColumns = [...cloneBoards.columns]
+
+    cloneColumns = applyDrag(cloneColumns, dropResult);
+    cloneBoards.columns = cloneColumns;
+    cloneBoards.columnOrder = cloneColumns.map(item => item.id);
+    setColumns(cloneColumns);
+    setBoards(cloneBoards);
+  }
+  // on drag card
+  const onCardDrop = (columnId, dropResult) => {
+    if(dropResult.addedIndex !== null || dropResult.removedIndex !== null) {
+      const cloneColumns = [...columns]
+      let currentColumn = cloneColumns.find(item => item.id === columnId);
+      currentColumn.cards = applyDrag(currentColumn.cards, dropResult);
+      currentColumn.cardOrder = currentColumn.cards.map(item => item.id);
+      setColumns(cloneColumns);
+    }
   }
   
   return (
@@ -46,6 +65,7 @@ function BoardContent() {
         columns.map(column =>(
         <Draggable key={v4()}>
           <Column
+          onCardDrop={onCardDrop}
           column={column}
           ></Column>
         </Draggable>
@@ -53,6 +73,14 @@ function BoardContent() {
         )
       }
     </Container>
+    <div
+    className='flex items-center gap-x-2 cursor-pointer group grow h-fit p-4 rounded-lg bg-gray-400 bg-opacity-30 text-white text-xl'
+    >
+    <svg className='group-hover:font-semibold h-6 w-6' xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+    </svg>
+    <span className='group-hover:font-semibold'>Add new column</span>
+    </div>
     </div>
   )
 }
